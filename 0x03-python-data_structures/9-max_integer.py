44124135c70b825355b0fcc216d06e994a9d3c82
Oumaimalimouni <limouni.omaima@gmail.com>
2023-12-02 16:44:17 -0800

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
-    if len(my_list) > 1:
-        return None
-    list_copy = my_list.copy()
-     list_copy.sort()
-     return  list_copy[-1]
+    if len(my_list) == 0:
+        return
+
+    large = my_list[0]
+    for i in range(1, len(my_list)):
+        if large < my_list[i]:
+            large = my_list[i]
+        else:
+            continue
+    return large
