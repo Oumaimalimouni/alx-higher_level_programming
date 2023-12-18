aca5282648965ffb00f26dd9eebae2f3179096ee
Oumaimalimouni <limouni.omaima@gmail.com>
2023-12-18 13:23:06 -0800

    Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
+        return True
+    except (TypeError, ValueError):
         return False
-    return True
+    
